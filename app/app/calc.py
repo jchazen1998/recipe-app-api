aa0bc65185d5fc603bdc9051cbe9ba4a17e9102c
Chazen, Jacob <Jacob.Chazen@viasat.com>
2022-06-23 14:46:57 -0700

    added wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,16 @@
+"""
+Calculator functions
+"""
+    
+def add(x, y):
+    """Add x and y and return result.
+
+    Args:
+        x (int): x integer
+        y (int): y integer
+    """
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
